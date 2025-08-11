c4e9f3abcaf592b321045ed5a1b9e27765aaabed
kaua-reichert <al.kaua.reichert@impatech.edu.br>
2025-08-11 15:38:33 -0300

    prog

---
+++ MYTOOLS.py
@@ -0,0 +1,16 @@
+PI_INT = 1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+E_INT = 7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274
+
+def pi_real(n):
+    global PI_INT
+    numero = int(str(PI_INT)[:n])
+    if int(str(PI_INT)[n+1]) > 5:
+        numero += 1
+    return float('3.'+str(numero))
+
+def e_real(n):
+    global E_INT
+    numero = int(str(E_INT)[:n])
+    if int(str(E_INT)[n+1]) > 5:
+        numero += 1
+    return float('2.'+str(numero))
---
+++ programa.py
@@ -0,0 +1,4 @@
+import MYTOOLS
+
+print(MYTOOLS.pi_real(4))
+print(MYTOOLS.e_real(10))
